ed26920b89da6f64da3fa62cf9bb93e9fd37509f
Ultimate88 <alejandrogonzalez88x@gmail.com>
2026-03-03 12:38:54 +0100

    feat: create users module

--- backend-ultimatewatch/src/app.module.ts
+++ backend-ultimatewatch/src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -24,6 +25,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         synchronize: true,
       }),
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],

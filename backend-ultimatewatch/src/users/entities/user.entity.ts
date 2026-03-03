d3e874d0cc87f3e99003f06daab7687dfcef4cc4
Ultimate88 <alejandrogonzalez88x@gmail.com>
2026-03-03 12:52:59 +0100

    feat: create user entity

--- backend-ultimatewatch/src/users/entities/user.entity.ts
+++ backend-ultimatewatch/src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ nullable: true })
+  imagePath: string;
+}

e95935fb5d56a42c63712f597f14434c91363364
Ultimate88 <alejandrogonzalez88x@gmail.com>
2026-03-03 13:26:10 +0100

    feat: implement authentication logic

--- backend-ultimatewatch/src/auth/auth.service.ts
+++ backend-ultimatewatch/src/auth/auth.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+
+type AuthInput = {
+  username: string;
+  password: string;
+};
+type SignInData = {
+  userId: number;
+  username: string;
+};
+type AuthResult = {
+  accessToken: string;
+  userId: number;
+  username: string;
+};
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  async authenticate(input: AuthInput): Promise<AuthResult> {
+    const user = await this.validateUser(input);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid username or password');
+    }
+
+    return {
+      accessToken: `token-for-user-${user.userId}`,
+      userId: user.userId,
+      username: user.username,
+    };
+  }
+
+  async validateUser(input: AuthInput): Promise<SignInData | null> {
+    const user = await this.userService.findByUsername(input.username);
+
+    if (user && user.password === input.password) {
+      return { userId: user.id, username: user.username };
+    }
+    return null;
+  }
+}

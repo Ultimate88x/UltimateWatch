67be35f39a35499e0dc4113437aa26d1f95ec8e3
Ultimate88 <alejandrogonzalez88x@gmail.com>
2026-03-04 01:06:19 +0100

    test: add user registration integration tests

--- backend-ultimatewatch/test/auth.e2e-spec.ts
+++ backend-ultimatewatch/test/auth.e2e-spec.ts
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, HttpStatus } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 describe('AuthController (e2e)', () => {
   let app: INestApplication;
@@ -15,6 +19,8 @@ describe('AuthController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    const userRepository = moduleFixture.get(getRepositoryToken(User));
+    await userRepository.delete({ username: 'newuser_e2e' });
   });
 
   it('/auth/login (POST) - Should login admin user', () => {
@@ -35,6 +41,21 @@ describe('AuthController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/auth/profile')
       .expect(HttpStatus.UNAUTHORIZED);
+  });
+
+  it('/auth/signup (POST) - Should register a new user and return token', () => {
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        username: 'newuser_e2e',
+        email: 'e2e@test.com',
+        password: 'password123',
+      })
+      .expect(HttpStatus.CREATED)
+      .expect((res) => {
+        expect(res.body).toHaveProperty('accessToken');
+        expect(res.body.username).toBe('newuser_e2e');
+      });
   });
 
   afterAll(async () => {

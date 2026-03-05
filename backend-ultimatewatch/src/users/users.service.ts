10b409a84253d6d60fe5394c7f5a7d4956b2f89c
Ultimate88 <alejandrogonzalez88x@gmail.com>
2026-03-05 12:34:49 +0100

    feat: add user deletion logic

--- backend-ultimatewatch/src/users/users.service.ts
+++ backend-ultimatewatch/src/users/users.service.ts
@@ -39,8 +39,8 @@ export class UsersService {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    return await this.userRepository.delete(id);
   }
 
   async findByUsername(username: string): Promise<User | null> {

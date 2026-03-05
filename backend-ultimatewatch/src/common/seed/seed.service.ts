7d0f8730684cece6800898383a4863a094b6e36a
Ultimate88 <alejandrogonzalez88x@gmail.com>
2026-03-05 22:44:38 +0100

    fix: change user seeder so that ir resets when bootloading

--- backend-ultimatewatch/src/common/seed/seed.service.ts
+++ backend-ultimatewatch/src/common/seed/seed.service.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable, OnApplicationBootstrap, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -21,13 +18,14 @@ export class SeedService implements OnApplicationBootstrap {
   }
 
   async runSeed() {
-    const count = await this.userRepository.count();
-
-    if (count > 0) return;
+    await this.userRepository.query(
+      'TRUNCATE TABLE "users" RESTART IDENTITY CASCADE',
+    );
 
     this.logger.log('Seeding database...');
 
     const testUser = this.userRepository.create({
+      id: 1,
       username: 'admin',
       email: 'admin@watch.com',
       password: await bcrypt.hash('123456', 10),

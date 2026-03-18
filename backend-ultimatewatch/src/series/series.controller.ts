401c293066280ccedbe8eafb29f30cbc8735dcab
Ultimate88 <alejandrogonzalez88x@gmail.com>
2026-03-18 19:12:38 +0100

    feat: add list tmdb series endpoint

--- backend-ultimatewatch/src/series/series.controller.ts
+++ backend-ultimatewatch/src/series/series.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { SeriesService } from './series.service';
+import { TmdbApiService } from 'src/tmdbapi/tmdbapi.service';
 
 @Controller('series')
 export class SeriesController {
-  constructor(private readonly seriesService: SeriesService) {}
+  constructor(
+    private readonly seriesService: SeriesService,
+    private readonly tmdbapiService: TmdbApiService,
+  ) {}
+
+  @Get('tmdb-list')
+  async getTmdbSeries() {
+    const data = await this.tmdbapiService.getSeriesListFromTmdb();
+    return data;
+  }
 }
--- backend-ultimatewatch/src/series/series.module.ts
+++ backend-ultimatewatch/src/series/series.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SeriesService } from './series.service';
 import { SeriesController } from './series.controller';
+import { TmdbapiModule } from 'src/tmdbapi/tmdbapi.module';
 
 @Module({
+  imports: [TmdbapiModule],
   controllers: [SeriesController],
   providers: [SeriesService],
 })

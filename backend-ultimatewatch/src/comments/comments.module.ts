5bb376c847a6f68da0821f8cf31be2a14df658a3
Ultimate88 <alejandrogonzalez88x@gmail.com>
2026-06-02 22:31:21 +0200

    test: add chat tests

--- backend-ultimatewatch/src/comments/comments.module.ts
+++ backend-ultimatewatch/src/comments/comments.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommentsService } from './comments.service';
-import { CommentsController } from './comments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 import { Comment } from './entities/comment.entity';
 import { MembersModule } from 'src/members/members.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Comment]), MembersModule],
-  controllers: [CommentsController],
   providers: [CommentsService],
   exports: [CommentsService],
 })
